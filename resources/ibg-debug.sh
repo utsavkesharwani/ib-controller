3db85788bbf4d4ea01d6c60df0d3263c1b4aa083
Jack Tang <jack@taodinet.com>
2017-02-16 17:17:01 +0800

    Update

--- resources/ibg-debug.sh
+++ resources/ibg-debug.sh
@@ -1,11 +1,11 @@
-TWSUSERID=
-TWSPASSWORD=
+TWSUSERID=ibzzy123
+TWSPASSWORD=ibzzy1234
 TWSAPIPORT=4001
 IBCTRLPORT=7462
 
-TWSDIR=../lib/ibg-b952.1a/
-TWSCP=$TWSDIR/jts.jar:$TWSDIR/total.2013.jar
+TWSDIR=../lib/ibg-b963
+TWSCP=$TWSDIR/log4j-core-2.5.jar:$TWSDIR/log4j-api-2.5.jar:$TWSDIR/twslaunch-963.jar:$TWSDIR/jts4launch-963.jar:$TWSDIR/rss.jar:$TWSDIR/hsqldb.jar:$TWSDIR/httpcore-4.3.jar:$TWSDIR/httpclient-4.3.jar:$TWSDIR/total.2015c.jar
 IBCINI='./IBController.ini'
 JAVAOPTS='-Xmx768M -XX:MaxPermSize=256M'
 
-java -cp  $TWSCP:./IBController-3.2.0-ibg-b952.1a.jar $JAVAOPTS ibcontroller.IBGatewayController $IBCINI $TWSUSERID $TWSPASSWORD $TWSAPIPORT live
+java -cp  $TWSCP:./IBController-3.2.0-ibg-b963.jar $JAVAOPTS ibcontroller.IBGatewayController $IBCINI $TWSUSERID $TWSPASSWORD $TWSAPIPORT paper
